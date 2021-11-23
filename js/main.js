d1506bfb98c92c2e02a628993ea248d9f4c73449
Andrew Li <achyt28@gmail.com>
2021-11-23 16:49:30 -0500

    track created using cubes

--- js/main.js
+++ js/main.js
@@ -12,11 +12,22 @@ renderer.setClearColor( 0xb7c3f3, 1);
 const light = new THREE.AmbientLight( 0xffffff ); // soft white light
 scene.add( light );
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+// global variables
+const start_position = 3;
+const end_position = -start_position;
 
+//function for creating a cube (size, x position, y rotation)
+function createCube(size, positionX, rotY = 0, color = 0xfbc851){
+    const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+    const material = new THREE.MeshBasicMaterial( { color: color } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = positionX;
+    cube.rotation.y = rotY;
+    scene.add( cube );
+    return cube;
+}
+
+//set camera position
 camera.position.z = 5;
 
 
@@ -44,6 +55,13 @@ class Doll{
     }
 
 }
+
+function createTrack(){
+    createCube({w: start_position * 2 + .2, h: 1.5, d: 1}, 0, 0, 0xe5a716).position.z = -1;
+    createCube({w: .2, h: 1.5, d: 1}, start_position, -.35);
+    createCube({w: .2, h: 1.5, d: 1}, end_position, .35);
+}
+createTrack()
 
 //make new doll object
 let doll = new Doll();
